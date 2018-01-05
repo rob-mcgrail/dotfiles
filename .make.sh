ed19dce06e1eec6fc211bb39d09795bbd6378822
Rob McGrail <rob.mcgrail@gmail.com>
2018-01-05 14:03:58 +1300

    Adding new irb config.

--- .make.sh
+++ .make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim bash_profile zshrc gitconfig zlogin"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim bash_profile zshrc gitconfig irbrc zlogin"    # list of files/folders to symlink in homedir
 
 ##########
 
